795f7c6fce4f7563b3088f000fd1874810973efe
fathah-work <abdulfathahkodag@gmail.com>
2024-08-10 11:35:13 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+<img src="https://github.com/user-attachments/assets/cf92e206-0cee-4f38-abfd-959c27ac3232"
+height="100px"
+/>
+
+# NEXT DRIVE CLIENT
+
+Upload and other utility functions for Clients using `NEXT DRIVE` as file server.
+
+
+
+## Install
+```
+npm install nextdrive-client
+```
+
+## Usage
+```ts
+import NextDriveUploader from 'nextdrive-client';
+
+const uploader = new NextDriveUploader({
+    rootUrl:"http://localhost:4000",
+    apiKey: process.ev.API_KEY,
+});
+
+const response = await uploader.upload({
+    folder: "products",
+    files: [File]
+});
+
+```
---
+++ src/uploader.ts
@@ -0,0 +1,53 @@
+
+export  class NextDriveUploader {
+
+    private api: string;
+    private root: string;
+
+    constructor({ rootUrl,
+        apiKey
+    }:
+        {
+            rootUrl: string,
+            apiKey: string,
+        }
+    ) {
+
+        this.api = apiKey;
+        this.root = rootUrl;
+
+    }
+
+    async upload(p:{
+        folder: string, files:  File[]
+    }) {
+        {
+            const finalUrl = this.root.endsWith("/")?this.root:`${this.root}/`
+            const URL = `${finalUrl}upload/${p.folder}`;
+
+            try {
+                const formData = new FormData();
+
+                for(const file of p.files){
+                    formData.append('files', file);
+                }
+
+                const resp = await fetch(URL, {
+                    method: "POST",
+                    body: formData,
+                    headers:{
+                        'x-api-key': this.api
+                    }
+                });
+                const data = await resp.json();
+                return data;
+            } catch (error) {
+                console.error("Upload Error:",error);
+                return { success: false, message: "Failed to upload" }
+
+            }
+
+
+        }
+    }
+}
